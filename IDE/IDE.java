0ccc9da0b35394b3bd70308f80229692c20950fa
dragoon-32 <70985582+dragoon-32@users.noreply.github.com>
2025-10-26 21:21:36 +0900

    Add files via upload

--- IDE/IDE.java
+++ IDE/IDE.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 //main 객체
 public class IDE {
@@ -10,39 +11,46 @@ public class IDE {
 //GUI 개발할 때를 대비하여 CLI 부분을 따로 떼놓음.
 //기능을 추가하려면 loop()의 switch문에서 동작 코드를, print_menu()에서 기능및 번호 설명 코드를 추가할것
 class CLI{
-    JavaFile Jfile;
+    Scanner scan = new Scanner(System.in);
+    JavaFile Jfile = new JavaFile();
     //Constructor
     CLI(){
         loop();
     }
     //CLI loop
     void loop(){
-        Scanner scan = new Scanner(System.in);
+        int action;
         while(true){
             print_menu();
-            int action = scan.nextInt();
+            action = scan.nextInt();
+            scan.nextLine();    //버퍼 비우기
 
             //기능을 추가하고 싶으면 여기에 case를 추가해주세요
             switch(action){
                 case 1:
-                    Jfile = upload_file(scan);
+                    upload_file();
+                    System.out.println(Jfile.check_path());
                     break;
                 case 2:
+                    Jfile.compile();
                     break;
                 case 3:
                     break;
                 case 4:
+                    Jfile.getName("");
                     break;
                 case 5:
                     break;
                 case 6:
                     System.out.println("GoodBye");
+                    scan.close();
                     return;
                 default:
                     System.out.println("We Do Not have your option.");
                     System.out.println("Please Try Again");
                     break;
             }
+            //System.out.println(scan.hasNextInt());
         }
     }
     void print_menu(){
@@ -57,20 +65,59 @@ class CLI{
         System.out.println("6. Exit");
         System.out.print(">");
     }
-    JavaFile upload_file(Scanner scan){
+    void upload_file(){
         String path;
-        System.out.print("Java File Path:");
+        System.out.print("Java File name:");
         path = scan.nextLine();
-        return new JavaFile(path);
+        System.out.println(path);
+        Jfile.getName(path);
     }
 }
 
 //자바 파일 객체
 class JavaFile{
-    String path;
+    String path = ".\\";
+    String name = "";
+    File ErrMsg = new File(".\\Error.txt");
+    ProcessBuilder proc = new ProcessBuilder("");
 
-    JavaFile(String S){
-        path = S;
+    JavaFile(){
+        if(!ErrMsg.exists()){
+            try{
+                ErrMsg.createNewFile();
+            }
+            catch(Exception e){
+                System.out.println("Error at creating Error log file");
+                System.exit(-1);
+            }
+        }
     }
-    JavaFile(){}
+    void getPath(String path){
+        this.path = path;
+    }
+    void getName(String name){
+        this.name = name;
+    }
+    Boolean check_path(){
+        return new File(path + "\\" + name).exists();
+    }
+    void compile(){
+        if(name.isEmpty()){
+            System.out.println("ERROR: There is no java file.");
+            return;
+        }
+        proc.command("cmd", "/c", "javac " + path + name);
+        proc.redirectError(ErrMsg);
+        try{
+            Process act = proc.start();
+            BufferedReader output = new BufferedReader(new InputStreamReader(act.getInputStream(), "MS949"));
+            String s;
+            while((s = output.readLine()) != null){
+                System.out.println(s);
+            }
+        }
+        catch(Exception e){
+            System.out.println("ERROR!!!\n" + e.getMessage());
+        }
+    }
 }
